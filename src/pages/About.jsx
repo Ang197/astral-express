3df1c65c54bdf414d429806a4dbc7a46638d44f9
Ang197 <angeroderp@gmail.com>
2024-02-10 13:00:40 -0800

    More css changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ function About() {
         <>
             <div className="pageContainer">
                 <div className="previewSection w3-animate-top">
-                        <h1>About us</h1>
+                        <p>About us</p>
                         <p className="description">
                             Astral Express is a resource website for Honkai Star Rail players.
                             Need information for characters or light cones? Astral Express is
@@ -15,7 +15,7 @@ function About() {
                             We can be able to provide the latest updates/patches, newest banners,
                             or any tutorials to help anyone start out their Honkai Star Rail journey.
                         </p>
-                        <h1>Credits</h1>
+                        <p>Credits</p>
                         <p className="creditsLink">
                             Github: <a className="githubLink" href="https://github.com/Ang197" target="_blank"> Ang197 </a>
                         </p>
