3e8f77a170fa5686f42ee9b870e095212963c6cf
Ang197 <angeroderp@gmail.com>
2024-05-29 11:20:06 -0700

    refactor: Apply same changes from character search to weapon search

--- src/components/Light_Cone_Search_Components/SearchFunction.jsx
+++ src/components/Light_Cone_Search_Components/SearchFunction.jsx
@@ -1,30 +1,38 @@
 import {useState} from 'react';
 import SearchBar from '../../layout/SearchBar'
 import SearchResults from './SearchResults'
-import SearchFormat from "../searchFormat"
+import SearchFormat from '../SearchFormat'
 
 function SearchFunction() {
     const [results, setResults] = useState([]);
-
+    const [errorMessage, setErrorMessage] = useState('');
+    
     const handleSearch = async (query) => {
         try{
-            let formattedQuery = SearchFormat(query)
+            setErrorMessage('') //Resets the error message and gets rid of it on screen
+            let formattedQuery = SearchFormat(query) //Replaces spaces and apostrophes to be a valid search
             const response = await fetch(`https://genshin.jmp.blue/weapons/${formattedQuery}`)
+
             const data = await response.json();
-            console.log('Response: ', data);
+
             if(!data || !data.name) {
                 throw new Error('Invalid response data');
             }
-            const dataArray = Array.isArray(data) ? data : [data];
+
+            const dataArray = Array.isArray(data) ? data : [data]; //Checks to see if data is an array, or assigns a new array for data
             setResults(dataArray);
+
         } catch (error) {
             console.error('Error fetching data: ', error)
+            setErrorMessage('Invalid result. Please enter a valid character name')
+            setResults([])
         }
     };
 
     return(
         <>
             <SearchBar onSearch={handleSearch}/>
+            {errorMessage && <p>{errorMessage}</p>}
             <SearchResults results={results}/>
         </>
     )
