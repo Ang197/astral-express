7057b3d08474df9bac6f3f491ea936c03a69e1c4
Ang197 <angeroderp@gmail.com>
2024-05-29 11:33:16 -0700

    refactor(characterSearch): Add Character Weapon Type result

--- src/components/Character_Search_Components/SearchResults.jsx
+++ src/components/Character_Search_Components/SearchResults.jsx
@@ -11,6 +11,7 @@ function Results(props){
                     <h1>{character.name}</h1>
                     <p>Rarity: {character.rarity} star</p>
                     <p>Vision: {character.vision}</p>
+                    <p>Weapon type: {character.weapon}</p>
                     <p>Birthday: {character.birthday}</p>
                     <p>{character.description}</p>
                     {/* <img src={character.name.availableImages} alt="Character Image"/> */}
